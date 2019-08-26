9eef80076c8ac21af116f7f19d6c3114dee0190a
DalwyX <dalwyx@gmail.com>
2019-08-26 14:05:40 +0300

    Add First Person Player

---
+++ Assets/Scripts/FPSCamera.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProjectMaze
+{
+    public class FPSCamera : MonoBehaviour
+    {
+        [SerializeField] private float sensitivity = 5f;
+        [SerializeField] private float smoothing = 2f;
+        private Transform character;
+        private Vector3 currentLook;
+        private Vector3 smoothMov;
+
+        private void Awake()
+        {
+            character = transform.parent;
+        }
+
+        private void Start()
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        private void Update()
+        {
+            float h = Input.GetAxis("Mouse X");
+            float v = Input.GetAxis("Mouse Y");
+
+            Vector3 rot = new Vector3(h, v) * Time.deltaTime;
+            currentLook += rot * sensitivity;
+            currentLook.y = Mathf.Clamp(currentLook.y, -90f, 90f);
+
+            smoothMov.x = Mathf.Lerp(smoothMov.x, rot.x, 1f / smoothing);
+            smoothMov.y = Mathf.Lerp(smoothMov.y, rot.y, 1f / smoothing);
+            currentLook += smoothMov;
+
+            transform.localRotation = Quaternion.AngleAxis(-currentLook.y, Vector3.right);
+            character.localRotation = Quaternion.AngleAxis(currentLook.x, character.transform.up);
+        }
+    } 
+}
---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProjectMaze
+{
+    public class Mover : MonoBehaviour
+    {
+        [SerializeField] private float speed = 5;
+
+        private void Update()
+        {
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
+
+            Vector3 dir = new Vector3(h, 0, v);
+
+            transform.Translate(dir * speed * Time.deltaTime);
+        }
+    } 
+}
