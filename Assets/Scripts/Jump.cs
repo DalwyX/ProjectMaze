11e0045335a8b6562999a401f52d7fe11e80811d
DalwyX <dalwyx@gmail.com>
2019-08-27 13:28:40 +0300

    Add jump mechanic

---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProjectMaze
+{
+    [RequireComponent(typeof(Rigidbody))]
+    [RequireComponent(typeof(Collider))]
+    public class Jump : MonoBehaviour
+    {
+        [SerializeField] private float jumpForce = 5;
+        private Rigidbody rb;
+        private Collider col;
+
+        private void Awake()
+        {
+            rb = GetComponent<Rigidbody>();
+            col = GetComponent<Collider>();
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                PerformJump();
+            }
+        }
+
+        private void PerformJump()
+        {
+            if (isGrounded())
+                rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            
+        }
+
+        private bool isGrounded()
+        {
+            return Physics.Raycast(transform.position, Vector3.down, col.bounds.extents.y + 0.1f);
+        }
+    } 
+}
