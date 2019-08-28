8277ab768aa2b4cf002fe4e824a29a15e0de0115
DalwyX <dalwyx@gmail.com>
2019-08-28 16:30:41 +0300

    Game Improvements

--- Assets/Scripts/General Components/Data/Variables/UIL.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-namespace GeneralComponents
-{
-    [CreateAssetMenu(fileName = "New UILayer Variable", menuName = "General Components / Variables / UILayer", order = 10)]
-    public class UIL : Variable<UILayer> { } 
-}
