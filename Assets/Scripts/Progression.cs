ac55958b93084ea67210f2e52765d5c3d134ef9f
DalwyX <dalwyx@gmail.com>
2019-08-29 10:22:41 +0300

    Add score system

---
+++ Assets/Scripts/Progression.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GeneralComponents;
+
+namespace ProjectMaze
+{
+    public class Progression : MonoBehaviour
+    {
+        [SerializeField] private List<int> targets = new List<int>();
+        [SerializeField] private Int currentScore;
+        [SerializeField] private Int currentTarget;
+
+        private void Start()
+        {
+            currentScore.value = 0;
+            currentTarget.value = targets[0];
+        }
+
+        public void AddPoint()
+        {
+            Debug.Log("хай");
+            if (currentScore != null && currentTarget != null)
+            {
+                currentScore.value++;
+                if (currentScore.value == currentTarget.value && targets.Count > 0)
+                {
+                    targets.RemoveAt(0);
+                    currentTarget.value = targets[0];
+                }
+            }
+        }
+    } 
+}
---
+++ Assets/Scripts/UI/CheckboxScore.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using GeneralComponents;
+
+namespace ProjectMaze
+{
+    public class CheckboxScore : MonoBehaviour
+    {
+        [SerializeField] private Int activeScore;
+        [SerializeField] private Int currentTarget;
+        [SerializeField] private Text activeScoreField;
+        [SerializeField] private Text targetScoreField;
+        private Image fillImage;
+
+        private void Awake()
+        {
+            fillImage = GetComponent<Image>();
+        }
+
+        public void UpdateScore()
+        {
+            if (activeScoreField != null && activeScore != null)
+            {
+                activeScoreField.text = activeScore.value.ToString();
+            }
+
+            if (targetScoreField != null && currentTarget != null)
+            {
+                targetScoreField.text = currentTarget.value.ToString();
+            }
+
+            if (fillImage != null && activeScore != null && currentTarget != null)
+            {
+                float p = activeScore.value / currentTarget.value;
+                fillImage.fillAmount = p;
+            }
+        }
+    } 
+}
