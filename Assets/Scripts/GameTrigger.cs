a6faf97896318414f11340898f6d94e19562f6e9
DalwyX <dalwyx@gmail.com>
2019-08-30 22:00:45 +0300

    Add game finish

---
+++ Assets/Scripts/GameTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GeneralComponents;
+
+namespace ProjectMaze
+{
+    [RequireComponent(typeof(Collider))]
+    public class GameTrigger : MonoBehaviour
+    {
+        [SerializeField] private UILEvent triggerEvent;
+        [SerializeField] private UILayer layerToLoad;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            triggerEvent?.Notify(layerToLoad);
+        }
+    } 
+}
